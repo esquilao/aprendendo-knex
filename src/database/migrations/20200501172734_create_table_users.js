6de95f387d4f2e7918f4ed1ca689a80db7797609
esquilao <pepo.pedrinho82@gmail.com>
2020-05-01 20:19:36 -0300

    continuando

--- src/database/migrations/20200501172734_create_table_users.js
+++ src/database/migrations/20200501172734_create_table_users.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
-};
+ exports.up = knex => knex.schema.createTable('users', table => {
+       table.increments('id');
+       table.text('username').unique().notNullable();
 
-exports.down = function(knex) {
-  
-};
+       table.timestamp('created_at').defaultTo(knex.fn.now());
+       table.timestamp('updated_at').defaultTo(knex.fn.now());
+   })
+
+
+ exports.down = knex => knex.schema.dropTable('users');
+
